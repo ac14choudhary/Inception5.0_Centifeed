7044425c1db1458bcf7eaef084667daad3002124
ac14choudhary <acayush14@gmail.com>
2021-04-07 09:35:04 +0530

    15th - modified Splash

--- Screens/Splash.js
+++ Screens/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import LottieView from 'lottie-react-native';
 import {
   TouchableOpacity,
   View,
@@ -7,16 +7,42 @@ import {
   Text,
   StatusBar,
   StyleSheet,
+  Dimensions,
+  ImageBackground,
 } from 'react-native';
+
+const height = Dimensions.get('screen').height;
+const width = Dimensions.get('screen').width;
 
 export default function Spalsh({navigation}) {
   setTimeout(() => {
-    navigation.navigate('Home');
+    navigation.replace('Home');
   }, 3000);
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}>
-      <Text style={styles.Welcome}>Welocme to the Splash Screen</Text>
+      <ImageBackground
+        source={require('../assets/BG.png')}
+        resizeMode={'cover'}
+        style={{
+          height: height,
+          width: width,
+          flex: 1,
+          justifyContent: 'center',
+        }}>
+        <View>
+          <LottieView
+            style={{
+              alignSelf: 'center',
+              width: width - 5,
+              height: height / 2.5,
+            }}
+            source={require('../assets/Centifeed.json')}
+            autoPlay
+            loop
+          />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
